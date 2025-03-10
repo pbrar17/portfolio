77513ac5a71539c09b4ac8f31914d6d99942e0f4
Pahul Brar <pahul.brar@gettyimages.com>
2025-03-09 22:12:03 -0600

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
-
+import Navbar from './components/NavBar/navbar';
+import Intro from './components/Intro/intro';
+import Footer from './components/Footer/footer';
+import Skills from './components/Skills/skills';
+import Examples from './components/Works/works';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      <Intro />
+      <Skills />
+      <Examples />
+      {/* <Contact /> */}
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer/footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './footer.css';
+
+const Footer = () => {
+    return (
+        <footer className='footer'>
+            Copyright &#169; 2025 Pahul Brar. All right reserved.
+        </footer>
+    );
+}
+
+export default Footer
