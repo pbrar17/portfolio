2bf752e3d6cb333770190eb1913e95d07f4d4e30
Pahul Brar <pahul.brar@gettyimages.com>
2025-03-19 17:46:52 -0600

    Added my skills

--- src/components/Intro/intro.js
+++ src/components/Intro/intro.js
@@ -7,9 +7,9 @@ const Intro = () => {
     return (
         <section id="intro">
             <div className="introContent">
-                <span className="hello">Hello,</span>
-                <span className="introText">I'm <span className="introName">Pahul</span> <br />Software Engineer</span>
-                {/* <p className="introPara">I am a skilled web designer with experience in creating<br />visually appealing and user friendly websites.</p> */}
+                {/* <span className="hello">Hello,</span> */}
+                <span className="introText">Hello, I'm <span className="introName">Pahul</span>.<br /></span>
+                <p className="introPara">I am a Software Engineer.</p>
                 <Link><button className="btn"><img src={btnImg} alt="Hire" className='btnImg'/> Hire Me</button></Link>
             </div>
             {/* <img src={bg} alt="Profile" className="bg" /> */}
--- src/components/Skills/skills.js
+++ src/components/Skills/skills.js
@@ -1,15 +1,102 @@
 import React from 'react';
 import './skills.css';
-import UIDesign from '../../assets/logo.png';
-import WebDesign from '../../assets/logo.png';
-import AppDesign from '../../assets/logo.png';
+
+import git from "../../Images/git.png"
+import CSS from "../../Images/css.png"
+import Spring from "../../Images/spring.png"
+import react from "../../Images/react.png"
+import next from "../../Images/next1.png"
+import mysql from "../../Images/mysql.svg"
+import express from "../../Images/express.png"
+import jest from "../../Images/jest.png"
+import typescript from "../../Images/typescript.png"
+import java from "../../Images/java.png"
+import js from "../../Images/js.png"
+import html from "../../Images/html.png"
+// import api from "../../Images/api.jpg"
+import python from "../../Images/python.png"
+import node from "../../Images/node.png"
 
 const Skills = () => {
     return (
         <section id='skills'>
-            <span className="skillTitle">What I do</span>
-            <span className="skillDesc">I am a skilled and passionate web designer with experience in creating visually appealing and user-friendly websites. I have a strong understanding of design and a keen eye for detail. I am proficient in HTML, CSS, and JavaScript, as well as design software such as Adobe Photoshop and Illustrator.</span>
-            <div className="skillBars">
+            <span className="skillTitle">About Me</span>
+            <span className="skillDesc">I'm a Hardworking, dedicated Software Engineer, capable of working with any tech stack to deliver the best product. My passion for coding has helped me navigate all hardships of life, and made me a better person, and an even better devolper. 
+                I am always thinking about making something better, somtimes my ambitions become hurdles, but I like to reach for the stars, if I fail I will just be among the planets! </span>
+
+            <section id="skills">
+            <h2 className="skillsTitle">My Skills</h2>
+            <div className="skillsGrid">
+                <div className="skillItem">
+                    <img src={next} alt="Next.js" />
+                    <p>Next.js</p>
+                </div>
+                <div className="skillItem">
+                    <img src={react} alt="React" />
+                    <p>React</p>
+                </div>
+                <div className="skillItem">
+                    <img src={Spring} alt="Spring Boot" />
+                    <p>Spring Boot</p>
+                </div>
+                <div className="skillItem">
+                    <img src={mysql} alt="MySQL" />
+                    <p>MySQL</p>
+                </div>
+                <div className="skillItem">
+                    <img src={node} alt="nodejs" />
+                    <p>Node.js</p>
+                </div>
+                {/* <div className="skillItem">
+                    <img src={api} alt="api" />
+                    <p>API</p>
+                </div> */}
+            </div>
+
+            <div className="skillsGrid">
+                <div className="skillItem">
+                    <img src={jest} alt="JEST" />
+                    <p>JEST</p>
+                </div>
+                <div className="skillItem">
+                    <img src={typescript} alt="typescript" />
+                    <p>Typescript</p>
+                </div>
+                <div className="skillItem">
+                    <img src={express} alt="Express" />
+                    <p>Express</p>
+                </div>
+                <div className="skillItem">
+                    <img src={java} alt="java" />
+                    <p>Java</p>
+                </div>
+            </div>
+            <div className="skillsGrid">
+                <div className="skillItem">
+                    <img src={html} alt="HTML" />
+                    <p>HTML</p>
+                </div>
+                <div className="skillItem">
+                    <img src={js} alt="javascript" />
+                    <p>JavaScript</p>
+                </div>
+                <div className="skillItem">
+                    <img src={CSS} alt="css" />
+                    <p>CSS</p>
+                </div>
+            </div>
+            <div className="skillsGrid">
+                <div className="skillItem">
+                    <img src={git} alt="git" />
+                    <p>GIT</p>
+                </div>
+                <div className="skillItem">
+                    <img src={python} alt="python" />
+                    <p>Python</p>
+                </div>
+            </div>
+        </section>
+            {/* <div className="skillBars">
                 <div className="skillBar">
                     <img src={UIDesign} alt="UIDesign" className="skillBarImg" />
                     <div className="skillBarText">
@@ -31,7 +118,7 @@ const Skills = () => {
                         <p>You can write text related to mobile app development.</p>
                     </div>
                 </div>
-            </div>
+            </div> */}
         </section>
     );
 }
